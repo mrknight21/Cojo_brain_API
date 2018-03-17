60395226ea59be159687ee4ae2a8afa8d02bf05d
mche618 <mche618@aucklanduni.ac.nz>
2018-03-17 16:03:54 +1300

    completing scraper. working on database backend working on user management implementing fire-base Admin SDK

--- config.py
+++ config.py
@@ -1,19 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+def testprint():
+    print("app/diet_alg/cal_score.py")
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    firebasekey = '{
-  "type": "service_account",
-  "project_id": "sustained-flux-162207",
-  "private_key_id": "dfc3b1c710d54446d14849ca117b9b097c325e14",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqBIj4yS9vE5SC\n6i3q1QROdGO2tCefccZ/6Kcp2L7Hj/6jQGeJyklLtJpNTIoM+PGttIZzG13geJPm\nqlJq3dFA/bapNj4zpXB11RxfmuPfqTS3TuSxFVYiJVieO6xOFqVTmy8xjarrfIaM\ne9g1N/jrkhgK6W2rAkVTnnupFTRzN6K0dL7MG9EHcfV+GQQceVy3g4/TMT5thHN3\nWJviyaMou42Elm0LhixpeHc/Z4SZwaheMzca/ftOVEMuyi+esp/F9vVpAy2D1Uyk\nqmz1FzycNRz7Jo9OVUIG0wprIlVJrQkgPUJUciYSu1pgNeSUQynlfoz6sayR3dHo\nWM2imjppAgMBAAECggEAVC8TrXBcwtjEoAAZhV49mCVMXGKe/fWlrp8B6pgOex0D\nHk2dFt6pZUmDX8QG1T6M2JB7RFKoLKY3wa7TSWQVIWOfRvD7YrJH2aiQuIwmyg/r\n8NopJb/lWtn4I71zZ2USLrA38Ybuu1R5AOWvnEZfRDGeIwULTvI8ZpeiLoo34tY6\nNJGF5aFAiPvpLQlp5MTrghGMJYFGTKKsNWXQ7u4+Z1zyCDgbxLPQZSSUJVjc7dgX\nIvLSPdGPI9PPxs0LV8LhzDCaOiSzFpjasUngG2W7Ye9Xjw6jJxpVUyViiCsg1X+H\nhMEqyqPpE58QGbTXi8F20KVmp16mLkBJMJbezlhl0wKBgQDaIkmMfuYl3pU/1JEa\n7vGUvb+mIeu4g1Ny3sWmYe++xTBFthaIYwIk5jasjrHJpOBoOm3f1CHQyR4CtLQq\nzveTZZ+dprf7nAZXLxF/h3vyuBpTpFKzSVASpYpQmI+/s4VwH5XStTRocGYCLzV3\n9v7n2sXlPPXjnMY731CVujTjBwKBgQDHh/+DAxqNkf6YZQnWmRPsCTwskQu+/fMR\nR9GZN3FzqLUj2PmYkCRoIP8ct1IcTOyaVm0S9BtMMvpeu+mofR2s5l7/6uoZme3l\nnwaNj0MXj+6Mn2yKUeLgbHoR3Ni/nys6+t21JhXQKTmpFBnaLtpcOXIfL3hMdXob\niGsTVDtrDwKBgQDL4NwparoljwKkZEMzjz1QroEf//hXvrcSWFEYD9WK0rcpkOVC\nmHd6kYlsgvp845OzF1l2qMjqYe+gy9DRahxQMd2b9iVkEBKFDkMmlTUuSRCiOKXr\nWIx0wTCj39QLcvk3MO5RwHe5XcSaKhGpjLv3bK0mc3HHSdKmRzUEnMOtpQKBgCae\nxzEHj8MpJ/s9S0szM+zy2KfTp09ffgWxPfuHBLmbRPxcSetisvlIsbhQL1908DLr\nWY9amlIEZ/ugKZjIJs9Jg8fLI2azKZ3RSwZpXVYZMNYdIXggY2aG+JWuhnGIkGiJ\nGBKw/XXeFOAKGPrnKLLDF7i7inBaKLldjUQfgKS3AoGAcPbiPV94koL1miHAdepL\nCNlmOR0DQfCZ6BSFm8SNZ+IOWxQg1bzMsuCxLY7qDXWWpJ5KbmhSbi1+hHE5+gvf\nUCVKsyUY7P/wXNUinNB+IsC9dVtySgrTDonXIrJ4K1kvtFjq0F3jmRDpfnp7VEjX\naklATv//yKAVrfomAwb4M1I=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-s1cgi@sustained-flux-162207.iam.gserviceaccount.com",
-  "client_id": "104485391492248334419",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-s1cgi%40sustained-flux-162207.iam.gserviceaccount.com"
-}'
+
+
--- scraper/rss_scrape_demo.py
+++ scraper/rss_scrape_demo.py
@@ -1,119 +1,120 @@
-import requests
-from bs4 import BeautifulSoup
 from newspaper import Article
 import newspaper
 import json
 import csv
 import datetime
 import feedparser
+import re
+from db_management.config import db_connect
+from firebase_admin import db
+import csv
+
+
+class Scraper:
+
+    def __init__(self, load_db = True, load_path = None, save_path= None):
+        self.load_path = load_path
+        self.save_path = save_path
+        self.news = []
+        self.source ={}
+        if(load_db == True):
+            self.ref = db.reference("/sources")
+            self.source = self.ref.get()
+        else:
+            with open(load_path, 'r') as f:
+                reader = csv.reader(f)
+                for name, link in list(reader):
+                    self.source[name]= link
+
+
+
+    def create_time_stamp(self):
+        now = datetime.datetime.now()
+        return now.strftime("%Y%m%d_%H%M")
+
+
+    def save_news_json(self):
+        with open(self.save_path, 'w') as outfile:
+            json.dump(self.news, outfile)
+
+    def db_news_push(self):
+        ref = db.reference("/news_pool")
+        jsform = {}
+        for news in self.news:
+            id = news['source']+" "+news['pubdate']
+            jsform[id] = news
+        ref.update(jsform)
+
+
+    ##using the built() function from the newspaper lib to scrape all news from a given homepage. It is powerful, but the result is not very stable, so will not use it that often.
+    # def download_newsite(url):
+    #     newsp = newspaper.build(url)
+    #     print("number of articles: ", len(newsp.articles))
+    #     news = []
+    #     for article in newsp.articles:
+    #         new = get_content(article)
+    #         news.append(new)
+    #     return news
+
+    ##use the python feedparser to get basic informations
+    def download_rss(self):
+        for key, value in self.source.items():
+            feed = feedparser.parse(value)
+            feed_entries = feed.entries
+            print("number of articles: ", len(feed_entries))
+            for entry in feed.entries:
+                new={}
+                new["title"] = entry.title
+                new["description"] = re.sub('<.*?>', '', entry.description).strip(' \t\n\r')
+                new["source"]= key
+                new["link"] = entry.link
+                new["pubdate"] = entry.published
+                new = {**new, **self.get_content(url=entry.link)}
+                print(entry.title)
+                self.news.append(new)
 
 
 
 
+        # resp = requests.get(url)
+        # print(resp)
+        # soup = BeautifulSoup(resp.content, features="lxml")
+        # items = soup.findAll('item')
+        # news_items = []
+        # for item in items:
+        #     print(item.link.text)
+             # news_item = {}
+             # news_item['title'] = item.title.text
+             # news_item['description'] = item.description.text
+             # news_item['link'] = item.link.text
+             # news_item['pubdate'] = item.pubdate.text
+             # article = Article(news_item['link'])
+             # article.download()
+             # print(item)
+             # article.parse()
+             # content = article.text
+             # news_item['content'] = content
+             # print(news_item['title'],  news_item['content'])
+             # news_items.append(news_item)
 
-def create_time_stamp():
-    now = datetime.datetime.now()
-    return now.strftime("%Y%m%d_%H%M")
+    def news_clear(self):
+        self.news = []
 
-
-def save_news_json(path, news):
-    with open(path, 'w') as outfile:
-        json.dump(news, outfile)
-
-
-
-
-def save_news_csv(path,news):
-    try:
-        with open(path, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=news[0].keys())
-            writer.writeheader()
-            for data in news:
-                writer.writerow(data)
-    except IOError as e:
-            print("I/O error({0}): {1}".format(e.errno, e.strerror))
-    return
-
-
-
-
-##using the built() function from the newspaper lib to scrape all news from a given homepage. It is powerful, but the result is not very stable, so will not use it that often.
-def download_newsite(url):
-    newsp = newspaper.build(url)
-    print("number of articles: ", len(newsp.articles))
-    news = []
-    for article in newsp.articles:
-        new = get_content(article)
-        news.append(new)
-    return news
-
-##use the python feedparser to get basic informations
-def download_rss(url):
-    feed = feedparser.parse(url)
-    feed_title = feed['feed']['title']
-    feed_entries = feed.entries
-    print("number of articles: ", len(feed_entries))
-    news = []
-    for entry in feed.entries:
-        new={}
-        new["description"] = entry.description
-        new["title"] = entry.title
-        new["link"] = entry.link
-        new["pubdate"] = entry.published
-        new = {**new, **get_content(entry.link)}
-        print(entry.title)
-        news.append(new)
-    return news
-
-
-
-
-    # resp = requests.get(url)
-    # print(resp)
-    # soup = BeautifulSoup(resp.content, features="lxml")
-    # items = soup.findAll('item')
-    # news_items = []
-    # for item in items:
-    #     print(item.link.text)
-         # news_item = {}
-         # news_item['title'] = item.title.text
-         # news_item['description'] = item.description.text
-         # news_item['link'] = item.link.text
-         # news_item['pubdate'] = item.pubdate.text
-         # article = Article(news_item['link'])
-         # article.download()
-         # print(item)
-         # article.parse()
-         # content = article.text
-         # news_item['content'] = content
-         # print(news_item['title'],  news_item['content'])
-         # news_items.append(news_item)
-
-
-
-##use newspaper package's Article object to dig into the contents, and do further analysis and tagging
-def get_content(url):
-    new = {}
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    content = article.text
-    new['kw'] = article.keywords
-    return new
-
-
-
+    ##use newspaper package's Article object to dig into the contents, and do further analysis and tagging
+    def get_content(self, url):
+        new = {}
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        content = article.text
+        new['kw'] = article.keywords
+        return new
 
 
 def main():
-    #collection = download_news("https://www.stuff.co.nz/")
-    #save_news_csv(collection, "scraper/news_dump.csv")
-    collection = download_rss("http://rss.nzherald.co")
-    print(len(collection))
-    save_news_json("heral_news_dump.json", collection)
-
-
+    scraper = Scraper(load_db=False, load_path="reuter_news_dump.json")
+    scraper.download_rss()
 
 
 if __name__ == "__main__":
