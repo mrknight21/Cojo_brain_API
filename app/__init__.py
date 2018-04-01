aa082deaa134371dc8af1f62de616685d07a9415
mche618 <mche618@aucklanduni.ac.nz>
2018-04-02 10:38:03 +1200

    shrink app to microservice

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,18 @@
 
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    pass
 
 
-from app import routes, models
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
