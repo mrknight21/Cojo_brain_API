a04f36261d867e71a9c32177380653f35df13dc2
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-10-04 12:58:55 +1300

    add tester template

---
+++ server/tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from datetime import datetime, timedelta
+import unittest
+from config import Config
+
+#
+# class TestConfig(Config):
+#     TESTING = True
+#     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+#     ELASTICSEARCH_URL = None
+#
+#
+# class UserModelCase(unittest.TestCase):
+#     def setUp(self):
+#         self.app = create_app(TestConfig)
+#         self.app_context = self.app.app_context()
+#         self.app_context.push()
+#         db.create_all()
+#
+#     def tearDown(self):
+#         db.session.remove()
+#         db.drop_all()
+#         self.app_context.pop()
+
+
+# if __name__ == '__main__':
+#     unittest.main(verbosity=2)
