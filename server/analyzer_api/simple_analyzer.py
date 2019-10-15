13a488570bcd988a8569961a30097a6691332f42
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-10-15 23:19:01 +1300

    add reading time estimation

--- server/analyzer_api/simple_analyzer.py
+++ server/analyzer_api/simple_analyzer.py
@@ -1,6 +1,7 @@
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from textblob import  TextBlob
 from utility.text_preprocessing import clean_text
+import numpy as np
 '''
 Argument:
     text: a long string
@@ -11,42 +12,44 @@ Argument:
 class SimpleAnalyzer(object):
     WPM = 200
 
-    def __init__(self, cleaned = False, bulk_text = None):
+    def __init__(self,  bulk_text, cleaned = False,):
         self.bulk_text = bulk_text
         self.cleaned = cleaned
-        if not cleaned:
-            self.text = self.standard_text_preprocessing(self.text)
+        if not self.cleaned:
+            self.bulk_text = self.standard_text_preprocessing(self.bulk_text)
             self.cleaned = True
 
-    def standard_text_preprocessing(self, text):
-        return clean_text(text)
+    def standard_text_preprocessing(self, bulk_text):
+        return list(map(lambda x: clean_text(x) ,bulk_text))
 
+    def single_ananlyze(self, text):
+        polarity_scores = []
+        subjectivity_scores = []
+        reading_time = self.estimate_reading_time(text)
+        blob = TextBlob(text)
+        for sent in blob.sentences:
+            polarity_scores.append(sent.sentiment.polarity)
+            subjectivity_scores.append(sent.sentiment.subjectivity)
+        p_avg = np.mean(polarity_scores)
+        s_avg = np.mean(subjectivity_scores)
+        return p_avg, s_avg, reading_time
 
+    def bulk_analyze(self):
+        if not self.cleaned:
+            self.bulk_text = self.standard_text_preprocessing(self.text)
+            self.cleaned = True
+        return [(self.single_ananlyze(text)) for text in self.bulk_text]
 
-
-
-    def ananlyze(self):
-        pass
-        # if not self.bulk_text: return []
-        # ananlyze()
-
-def estimate_reading_time(text):
-    text = clean_text(text)
-    words  = word_tokenize(text)
-    print(words)
-    est_reading_ms = 200*len(words)
-    return round(est_reading_ms/1000)
+    def estimate_reading_time(self, text):
+        text = clean_text(text)
+        words = word_tokenize(text)
+        est_reading_ms = 200*len(words)
+        return round(est_reading_ms/1000)
 
 def tester():
-    text = 'bryan is a bad boy. He is going to the states.'
-    blob = TextBlob(text)
-    tags = blob.tags
-    print(tags)
-    polarity = blob.sentiment[0]
-    subjectivity = blob.sentiment[1]
-    print('polarity score: {}'.format(polarity))
-    print('subjectivity score: {}'.format(subjectivity))
-    print("estimated reading time: {} s".format(estimate_reading_time(text)))
+    text = ['bryan is a bad boy. He is going to the states.', 'Jamse is coming back to NZ. And jojo is gonna start a party']
+    analyzer = SimpleAnalyzer(text)
+    print(analyzer.bulk_analyze())
 
 if __name__ == "__main__":
     tester()
