650dd367c36338b332210f1b91acf3bf231551b3
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-11-21 18:33:31 +1300

    remove bulk analysis

--- server/analyzer_api/simple_analyzer.py
+++ server/analyzer_api/simple_analyzer.py
@@ -12,44 +12,49 @@ Argument:
 class SimpleAnalyzer(object):
     WPM = 200
 
-    def __init__(self,  bulk_text, cleaned = False,):
-        self.bulk_text = bulk_text
+    def __init__(self,  cleaned = False,):
+        # self.bulk_text = bulk_text
         self.cleaned = cleaned
-        if not self.cleaned:
-            self.bulk_text = self.standard_text_preprocessing(self.bulk_text)
-            self.cleaned = True
+        # if not self.cleaned:
+        #     self.bulk_text = self.standard_text_preprocessing(self.bulk_text)
+        #     self.cleaned = True
 
-    def standard_text_preprocessing(self, bulk_text):
+    def single_text_preprocessing(self, text):
+        text = clean_text(text)
+        return text
+
+    def bulk_text_preprocessing(self, bulk_text):
         return list(map(lambda x: clean_text(x) ,bulk_text))
 
-    def single_ananlyze(self, text):
+    def single_analyze(self, text):
+        text = self.single_text_preprocessing(text)
+        insight = {'polarity':0.0, 'subjectivity': 0.0, 'reading_time': 0, 'words_count':0}
+        insight['polarity'], insight['subjectivity'] = self.text_blob_scoring(text)
+        insight['reading_time'], insight['words_count'] = self.estimate_reading_time(text)
+        return insight
+
+    def text_blob_scoring(self, text):
         polarity_scores = []
         subjectivity_scores = []
-        reading_time = self.estimate_reading_time(text)
+
         blob = TextBlob(text)
         for sent in blob.sentences:
             polarity_scores.append(sent.sentiment.polarity)
             subjectivity_scores.append(sent.sentiment.subjectivity)
-        p_avg = np.mean(polarity_scores)
-        s_avg = np.mean(subjectivity_scores)
-        return p_avg, s_avg, reading_time
+        p_avg = round(np.mean(polarity_scores), 2)
+        s_avg = round(np.mean(subjectivity_scores), 2)
+        return p_avg, s_avg
 
-    def bulk_analyze(self):
-        if not self.cleaned:
-            self.bulk_text = self.standard_text_preprocessing(self.text)
-            self.cleaned = True
-        return [(self.single_ananlyze(text)) for text in self.bulk_text]
 
     def estimate_reading_time(self, text):
         text = clean_text(text)
         words = word_tokenize(text)
         est_reading_ms = 200*len(words)
-        return round(est_reading_ms/1000)
-
+        return round(est_reading_ms/1000), len(words)
 def tester():
-    text = ['bryan is a bad boy. He is going to the states.', 'Jamse is coming back to NZ. And jojo is gonna start a party']
-    analyzer = SimpleAnalyzer(text)
-    print(analyzer.bulk_analyze())
+    text = 'bryan is a bad boy. He is going to the states. Jamse is coming back to NZ. And jojo is gonna start a party'
+    analyzer = SimpleAnalyzer()
+    print(analyzer.single_analyze(text))
 
 if __name__ == "__main__":
     tester()
