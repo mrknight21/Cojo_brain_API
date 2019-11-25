6380fa4eab6b03bfa68e92178058cc0333df7180
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-11-24 23:29:19 -0600

    debug: json serialization issue and time parsing

--- server/news_collector/news.py
+++ server/news_collector/news.py
@@ -26,26 +26,44 @@ news_obj = {
 from newsplease import NewsPlease
 import json
 import hashlib
+from datetime import datetime
 
 class News(object):
 
-    def __init__(self, raw_info, url, download_time):
+    def __init__(self, raw_info, url, download_time, use_newsapi = True):
         self.raw_info = raw_info
         self.news_id = self.generate_news_id(raw_info)
         self.url = url
         self.info_dic = None
-        self.source = None
-        self.author = None
-        self.title = None
-        self.description = None
-        self.image_url = None
-        self.published_time = None
-        self.download_time = None
-        self.tag = {}
-        self.bag = {}
+        self.source = self.raw_info['source']
+        self.author = self.raw_info['author']
+        self.title = self.raw_info['title']
+        self.description = self.raw_info['description']
+        self.image_url = self.raw_info['urlToImage']
+        if use_newsapi:
+            self.published_time = self.parse_api_time_str(self.raw_info['publishedAt'])
+        else:
+            self.published_time = None
+        self.download_time = download_time
+        self.tags = {}
+        self.bags = {}
+        self.short_content = self.raw_info['content']
         self.main_category = "general"
         self.categories = []
+        self.article_obj = None
 
+    def parse_api_time_str(self, time_str):
+        '''
+        :param time_str: a string from api timestamp, e.g. 2019-09-23T08:48:54Z
+        :return: a python datetime object
+        '''
+        time_str = time_str[:19]
+        try:
+            time_obj = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S')
+            return time_obj
+        except ValueError as e:
+            print(time_str+ " " +e)
+            return None
 
     def to_dic(self):
         self.info_dic = {
@@ -59,16 +77,18 @@ class News(object):
             'publishedAt': self.published_time,
             'downloadedAt': self.download_time,
             'tag': self.tags,
-            'bag': self.bag,
+            'bag': self.bags,
             'main_category': self.main_category,
             'categories': self.categories
         }
     def out_put_json(self):
         if self.info_dic:
-            json_obj = json.dump(self.info_dic)
+            json_obj = json.dumps(self.info_dic)
             return json_obj
         else:
-            return
+            self.to_dic()
+            json_obj = json.dumps(self.info_dic)
+            return json_obj
 
     def get_article_obj(self):
         self.article_obj = NewsPlease.from_url(self.url)
@@ -84,7 +104,7 @@ class News(object):
         # salt = 'bryanhandsome'
         concat_str = title + str(url)
         hashed_id = hashlib.sha224(concat_str.encode('utf-8')).digest()
-        return hashed_id
+        return str(hashed_id)
 
 
     def fill_object_with_news_please(self):
