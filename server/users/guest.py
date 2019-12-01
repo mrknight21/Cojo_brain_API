4f4c4ddc92551e9ed0bdc9b4d399c8e88c815743
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-01 01:02:41 -0600

    create guest class

---
+++ server/users/guest.py
@@ -0,0 +1,17 @@
+from user import User
+
+class Guest(User):
+
+    def __init__(self, user_id, time = None, location = None, page_index = 1):
+        super()._init_(user_id)
+
+    def retreive_from_cache(self, user_id):
+        pass
+
+
+def main():
+    print ('hello')
+
+
+if __name__ == '__main__':
+    main()
