0da5aa2815a6bdca1905f9620b8d50cf03c75e77
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-01 01:11:57 -0600

    create news retreiver class

---
+++ server/news_processes/newsretreiver.py
@@ -0,0 +1,17 @@
+
+import json
+import datetime
+from news_processes.news import News
+
+
+class NewsCollector(object):
+    
+
+    def __init__(self, mongo_db, news_api_key = None, max_load = 2000, analyzers = [], save = False, *argv, **kwas):
+        self.max_load = max_load
+        self.mongo_db = mongo_db
+        self.analyzers = analyzers
+        self.now = datetime.datetime.utcnow()
+
+    def retreive_news(self, page_cache):
+        return []
