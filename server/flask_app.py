d45c8d754cca1f03c19bb68229a5aa3f276e11cf
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-01 01:18:21 -0600

    half complete newsfeed api with guest and member view

--- server/flask_app.py
+++ server/flask_app.py
@@ -8,9 +8,8 @@ from flask import Flask, request
 from config import Config
 # from flasgger import Swagger
 import os, sys
-# from users import  User
-import numpy as np
-import pandas as pd
+from users import  *
+from news_processes.newsretreiver import NewsRetreiver
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(basedir)
@@ -19,13 +18,50 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # swagger = Swagger(app)
 
+
 @app.route('/asknews', methods=["POST"])
 def get_newsfeed():
+# Argument for request.form:
+# user_id : string
+# time: 2019-11-30T16:14:00Z
+#
+# Return:
+# array:
+# [
+#   news_json_objects
+# ]
+# logic:
+# 1. classify user as member or guests
+# 2. get user cache_news
+# 3. If no cache_news or if news expire: create news cache, and set page_id to 1
+# 4. If valid news cache retreived, then retreive news base on news id
+# 5. return news after postprocessing
+
+    is_member = False
     if request.method == 'POST':
-        user_id = request.form['user_id']
-        cur_time = request.form['time']
-        cur_page = request.form['cur_page']
-        return 'user_id'.format(user_id) + " at:{}".format(cur_time) + "page_at: {}".format(cur_page)
+        if 'user_id' in request.form and request.form['user_id']:
+            user_id = request.form['user_id']
+            auth_token = request.form['auth_token']
+            cur_time = request.form['time']
+            if 'cur_page' in request.form and request.form['cur_page']:
+                cur_page = request.form['cur_page']
+            else:
+                cur_page = 1
+            cur_user = Member(user_id, auth_token, cur_time)
+            if cur_user.has_loggin:
+                page_cache = cur_user.retreive_from_cache()
+                if not page_cache or not cur_user.cache_validate(page_cache):
+                    page_cache = cur_user.rank_news()
+                news_retreiver = NewsRetreiver()
+                news = news_retreiver.retreive_news(page_cache)
+                return news
+            else:
+                return cur_user.return_non_loggin_info()
+
+
+
+
+
 
 if __name__ == '__main__':
     print(Config.NEWS_API_KEY)
