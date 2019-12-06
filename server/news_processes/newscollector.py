e4c2945d027b14c4b2e5c292c876d0e3f1493c08
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-06 01:07:04 -0600

    get and scrape accordingly to category and country from api

--- server/news_processes/newscollector.py
+++ server/news_processes/newscollector.py
@@ -13,7 +13,7 @@ class NewsCollector(object):
 
     CATEGORY = ['business', 'entertainment', 'general', 'health', 'science', 'sports' ,'technology']
 
-    def __init__(self, mongo_db, news_api_key = None, max_load = 2000, analyzers = [], save = False, *argv, **kwas):
+    def __init__(self, mongo_db, news_api_key = None, max_load = 2000, analyzers = [], save = False, country = None,*argv, **kwas):
         if news_api_key:
             self.news_api = NewsApiClient(api_key=news_api_key)
         elif Config.NEWS_API_KEY:
@@ -21,41 +21,42 @@ class NewsCollector(object):
         else:
             raise Exception('No API KEY')
         self.save = save
+        self.country = country
         self.max_load = max_load
         self.mongo_db = mongo_db
         self.analyzers = analyzers
         self.now = datetime.datetime.utcnow()
         if 'sources' not in kwas:
             self.sources =self.get_country_source_str()
-        # self.hasher = eval('hashlib.'+Config.NEWS_ID_HASH)
-        # self.salt = Config.HASH_SECRET_SALT
 
-    def collect_news(self, mode='headlines', params = None):
+    def collect_news(self, mode='headlines', country = None, category = None):
         processed_news_count = 0
         total_news_count = 0
         cur_page = 1
         total = 0
         download_time = self.now
-        bacth_collection = self.get_batch_news_api_call(mode = mode, page=cur_page)
+        bacth_collection = self.get_batch_news_api_call(mode = mode, page=cur_page, country = country,category = category)
         status = False
         if bacth_collection['status'] == 'ok':
             status = True
             total_news_count = bacth_collection['totalResults']
         else:
             raise Exception('Abnormal response from API. code: {}'.format(bacth_collection['status']))
-
+        # Looping through the pages from the API
         while(status and processed_news_count < total_news_count):
             if cur_page > 1:
-                bacth_collection = self.get_batch_news_api_call(mode = mode, page=cur_page)
+                bacth_collection = self.get_batch_news_api_call(mode = mode, page=cur_page, country = country, category = category)
                 download_time = datetime.datetime.utcnow()
             batch_news =  bacth_collection['articles']
-            parsed_news_data, flag = self.batch_process_news(batch_news, download_time = download_time)
+            parsed_news_data, flag = self.batch_process_news(batch_news, download_time = download_time, country = country, category = category)
             if flag and self.save:
                 self.mongo_news_dump(parsed_news_data)
             processed_news_count += len(batch_news)
             cur_page += 1
 
-    def get_country_source_str(self, country = 'us'):
+    def get_country_source_str(self, country = None):
+        if not country:
+            country = self.country
         sources_ids = []
         src_str = ""
         source_info = self.news_api.get_sources(country=country)
@@ -68,27 +69,35 @@ class NewsCollector(object):
             src_str = ','.join(sources_ids)
         return src_str
 
-    def get_batch_news_api_call(self, mode = "headlines", page= 1):
+    def get_batch_news_api_call(self, mode = "headlines", page= 1, country = None, category = None):
         collection = {}
         if mode == 'headlines':
             # /v2/top-headlines
             # for api params, see: https://newsapi.org/docs/endpoints/top-headlines
-            collection = self.news_api.get_top_headlines(sources=self.sources, page_size=100, page=page)
+            if not country:
+                collection = self.news_api.get_top_headlines(sources=self.sources, page_size=100, page=page, category = category)
+            else:
+                collection = self.news_api.get_top_headlines(country = country, page_size=100, page=page, category=category)
         if mode == 'general':
             # # /v2/everything
             # for api params, see: https://newsapi.org/docs/endpoints/sources
-            collection = self.news_api.get_everything(sources=self.sources, sort_by='relevancy', page=page)
+            if not country:
+                collection = self.news_api.get_everything(sources=self.sources, sort_by='relevancy', page=page, category = category)
+            else:
+                collection = self.news_api.get_everything(country = country, sort_by='relevancy', page=page, category = category)
         return collection
 
 
-    def batch_process_news(self, batch_raw_content, download_time = None, thread = 5):
+    def batch_process_news(self, batch_raw_content, download_time = None, country = None, category = None, thread = 5):
+        if not country:
+            country = self.country
         batch_container = []
         flag = True
         if not download_time:
             download_time = datetime.datetime.utcnow()
         for news_info in batch_raw_content:
             try:
-                news = News(news_info, news_info['url'], download_time)
+                news = News(news_info, news_info['url'], download_time, country= country, category = category)
                 content = news.get_article_full_text()
                 news.tags = self.tag_news(content)
                 news.to_dic()
@@ -121,17 +130,14 @@ class NewsCollector(object):
 
 
 def tester():
+    countries = ['us', 'nz', 'uk']
     mongo_conn = Mongo_conn()
     simple_analyzer = SimpleAnalyzer()
-    collector  = NewsCollector(mongo_conn, analyzers=[simple_analyzer], save = True)
+    collector  = NewsCollector(mongo_conn, analyzers=[simple_analyzer], save = True, country = 'us')
     print(collector.sources)
-    collector.collect_news()
-    # news = collector.collect_news()
-    # if news:
-    #     print(len(news))
-    #     ids = list(map( lambda  x: NewsCollector.generate_id(x), news))
-    #     print(ids)
-
+    for c in countries:
+        for cat in collector.CATEGORY:
+            collector.collect_news(country = c, category= cat)
 
 if __name__ == "__main__":
     tester()
