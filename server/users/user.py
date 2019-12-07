5f9bb531b2889dd5160f9ed8cb59bed34569d383
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-07 01:19:03 -0600

    remove abtract method

--- server/users/user.py
+++ server/users/user.py
@@ -13,7 +13,3 @@ class User(ABC):
 
     def __init__(self, user_id, time = None, location = None, page_index = 1):
         self.user_id = user_id
-
-    @abstractmethod
-    def retreive_from_cache(self, user_id):
-        pass
