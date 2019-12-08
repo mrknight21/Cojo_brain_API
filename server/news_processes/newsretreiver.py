adf3f4fd91543149222a8fd348928b52365fb296
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2019-12-08 01:36:15 -0600

    complete constructing page news json pipeline

--- server/news_processes/newsretreiver.py
+++ server/news_processes/newsretreiver.py
@@ -2,18 +2,56 @@
 import json
 import datetime
 from news_processes.news import News
-
+from bson.objectid import ObjectId
 
 class NewsRetreiver(object):
-
 
     def __init__(self, mongo_db, news_api_key = None, max_load = 2000, analyzers = [], save = False, *argv, **kwas):
         self.max_load = max_load
         self.mongo_db = mongo_db
         self.now = datetime.datetime.utcnow()
 
-    def retreive_news(self, news_ids):
-        return []
+    def construct_page_json(self, page_cache):
+        json_ranked_news = None
+        page, news_ids = self.parsing_single_page_cache(page_cache)
+        news_lst = self.retreive_news(news_ids)
+        if news_lst:
+            for news_obj in news_lst:
+                try:
+                    id = news_obj['_id']
+                    page[id].update(news_obj)
+                    page[id]['_id'] = str(page[id]['_id'])
+                    page[id]['publishedAt'] = self.time_parser(page[id]['publishedAt'])
+                    page[id]['downloadedAt'] = self.time_parser(page[id]['downloadedAt'])
+                except Exception:
+                    pass
+                if not self.validate_news_obj(page[id]):
+                    del page[id]
+            ranked_news = sorted(page.values(), key = lambda x: x['rank'])
+            json_ranked_news = json.dumps(ranked_news)
+        return json_ranked_news
 
-    def time_parser(self, time_obj):
-        return time_obj.strftime("%m/%d/%Y, %H:%M:%S")
+    def retreive_news(self, news_ids):
+        assert isinstance(news_ids[0], ObjectId)
+        query = { '_id': {'$in': news_ids}}
+        try:
+            return self.mongo_db.find_many('News_pool', query)
+        except Exception as e:
+            print(e)
+            return None
+
+    def parsing_single_page_cache(self, page_cache):
+        page = { news_cache['_id']:{'_id':str(news_cache['_id']) ,'rank': rank, 'interaction':news_cache['interaction']} for rank, news_cache in enumerate(page_cache)}
+        news_ids = list(page.keys())
+        return page, news_ids
+
+    def time_parser(self, time_obj, time_format = "%m/%d/%Y, %H:%M:%S"):
+        return time_obj.strftime(time_format)
+
+    def validate_news_obj(self, news_obj):
+        valid = True
+        if not news_obj['_id']: valid = False
+        if not news_obj['news_id']: valid = False
+        if not news_obj['url']: valid = False
+        if not news_obj['main_category']: valid = False
+        return valid
