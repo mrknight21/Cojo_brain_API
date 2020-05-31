00edcc7ee56f13dd8fdf9f640a27ca2f93e4c734
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2020-05-31 00:46:56 -0500

    update wsgi

--- server/cojo_app.py
+++ server/cojo_app.py
@@ -1,8 +1,8 @@
 from app import create_app
 from config import Config
 
-app = create_app()
+application = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=Config.PORT)
+    application.run(host='0.0.0.0', port=Config.PORT)
--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,4 @@
-from application import app
+from cojo_app import application
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
